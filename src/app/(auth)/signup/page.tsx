9d0e2572fed3ca6ca1a8c1a5c661a176551bdadb
vishalgupta <vishalgupta91998@gmail.com>
2024-11-15 11:08:45 +0530

    test

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -21,7 +21,7 @@ const Signup = () => {
     }
   
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/signup`, {
+      const response = await fetch(`https://hnfbackend-production.up.railway.app/api/v1/auth/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
