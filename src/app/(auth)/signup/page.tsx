df91bef55c820c25497d016687b3d7a4f438b675
vishalgupta <vishalgupta91998@gmail.com>
2024-11-15 11:19:05 +0530

    env commited

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -21,6 +21,8 @@ const Signup = () => {
     }
   
     try {
+      console.log("only envv ",`${process.env.NEXT_PUBLIC_BACKEND_URL}`)
+      console.log("path is " ,`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/signup` )
       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/signup`, {
         method: 'POST',
         headers: {
